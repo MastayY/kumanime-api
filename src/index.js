acd5ee4a01d324a1fb098028241847ad0be82633
Mastay <nasywanfadilah@gmail.com>
2024-01-30 20:52:50 +0700

    update header request

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import router from './routes/index.js';
 import cors from 'cors';
 import axios from 'axios';
 
-const userAgent = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
-    'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
-]
-
 const app = express();
 const port = 3000;
 
 app.use(cors());
 axios.defaults.validateStatus = () => true;
-axios.defaults.headers.common['User-Agent'] = userAgent[Math.random() * userAgent.length | 0];
+axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36';
 
 app.use('/api', router)
 app.use('/', (req, res) => {
