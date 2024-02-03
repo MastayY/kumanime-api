ea556fa584c32daadb203f006cdbccc4542f015b
Mastay <nasywanfadilah@gmail.com>
2024-02-03 22:40:29 +0700

    fix: update header request

--- src/utils/index.js
+++ src/utils/index.js
@@ -41,6 +41,6 @@ export const getData = async (url) => {
 export const headers = {
     "Referer": BASE_URL,
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7,eu;q=0.6",
     "Accept-Encoding": "gzip,deflate",
 }
